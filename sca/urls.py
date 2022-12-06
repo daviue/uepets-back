5150ef3d32e907908de44c44d6159d5bf58afff4
Davi Rodrigues <davi.rodrigues@helpper.com.br>
2022-12-06 15:57:49 -0300

    fix: adding prefix api path

--- sca/urls.py
+++ sca/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from aplic.views import PetsViewSet, UsuariosViewSet, OngsViewSet, EnderecosViewSet, VacinasViewSet, IndexView
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
-router.register(r'pets', PetsViewSet)
-router.register(r'usuarios', UsuariosViewSet)
-router.register(r'ongs', OngsViewSet)
-router.register(r'enderecos', EnderecosViewSet)
-router.register(r'vacinas', VacinasViewSet)
+router.register(r'api/pets', PetsViewSet)
+router.register(r'api/usuarios', UsuariosViewSet)
+router.register(r'api/ongs', OngsViewSet)
+router.register(r'api/enderecos', EnderecosViewSet)
+router.register(r'api/vacinas', VacinasViewSet)
 
 
 urlpatterns = [
